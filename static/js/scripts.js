4e0c8786020c1e164d7a4873cffd8708e6287ea2
Jorge Ferreiro <me@jgferreiro.com>
2015-04-25 18:44:40 +0200

    Adding Search

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,16 @@
 // TOBE IMPLEMENTED
 var mobileMenu = $('#js-navbar-menu'),
-	menu = $('.headerMenu');
+	menu = $('.headerMenu'),
+	mobileSearch = $('#js-navbar-search'),
+	mobileSearchForm = $('.headerSearch');
 
 mobileMenu.click(function() {
+	mobileSearchForm.hide("fast");
 	mobileMenu.toggleClass('pressed');
 	menu.slideToggle("fast");
+});
+
+mobileSearch.click(function() {
+	menu.hide(0);
+	mobileSearchForm.fadeToggle("fast");
 });
